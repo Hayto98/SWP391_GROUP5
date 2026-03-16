44381440502d779bf0fe8c5706cf3f27ffbc2f75
Hayto98 <haytouchiha861@gmail.com>
2026-03-16 20:28:21 +0700

    update

--- BE/src/index.js
+++ BE/src/index.js
@@ -41,12 +41,8 @@ app.use('/api/enterprise', enterpriseRoutes)
 app.use('/api/reports', require('./routes/wasteReportRoutes'))
 app.use('/api/waste-types', require('./routes/wasteTypeRoutes'))
 app.use('/enterprise', require('./routes/enterpriseRoutes'))
-<<<<<<< HEAD
-app.use('/citizen', citizenRoutes)
-=======
 app.use('/citizen', require('./routes/citizenRoutes'))
 app.use('/vouchers', require('./routes/voucherRoutes'))
->>>>>>> dev
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' })
