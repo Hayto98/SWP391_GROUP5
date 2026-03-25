154232fe2d40c020c1b287538e6560c90fe751d5
Nguyen Do Bao Hung <hungnguyen9999123@gmail.com>
2026-03-25 14:32:47 +0700

    fix login

--- FE/src/services/apiClient.js
+++ FE/src/services/apiClient.js
@@ -27,7 +27,7 @@ const apiClient = axios.create({
 apiClient.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error?.response?.status === 401) {
+    if (error?.response?.status === 401 && !window.location.pathname.includes("/login")) {
       localStorage.clear();
       window.location.href = "/login";
     }
