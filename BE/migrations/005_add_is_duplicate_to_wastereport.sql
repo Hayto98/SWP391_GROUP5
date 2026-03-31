dbfcfe4639b9a1939883b767f4dfc7dd94cb3fc0
ngothuan1112004@gmail.com <ngothuan1112004@gmail.com>
2026-03-31 20:02:10 +0700

    xóa db

--- BE/migrations/005_add_is_duplicate_to_wastereport.sql
+++
@@ -1,5 +0,0 @@
--- Add is_duplicate column to wastereport table to support spam prevention
--- This column flags whether a report is considered a duplicate of another recent report
-
-ALTER TABLE wastereport
-ADD COLUMN IF NOT EXISTS is_duplicate TINYINT(1) DEFAULT 0 COMMENT '1 = duplicate, 0 = valid';
